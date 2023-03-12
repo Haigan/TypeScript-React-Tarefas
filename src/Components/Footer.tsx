e797901523d818b0c462556a3f106036eca8f93d
Haigan <haiganbr@gmail.com>
2023-03-11 21:37:00 -0300

    Projeto CRUD React + TypeScript

---
+++ src/Components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>
+        <span>React + TS todo</span> @ 2023
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
